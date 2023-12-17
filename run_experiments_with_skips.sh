4ae6d791b9acd55345ca4a6eb77255b7c0a7a518
Daniil Shlyonskiy <d-shlenskii@terranova.vla.yp-c.yandex.net>
2023-12-17 23:21:47 +0300

    experiments with skips

--- run_experiments_with_skips.sh
+++ run_experiments_with_skips.sh
@@ -5,19 +5,19 @@ SIGMA_MAX=80.0
 DS_PARAMS_DIR="gaussian_params"
 
 BATCH=1024
-SEEDS="0-49999"
-NUM=10000
 
-CUDA_VISIBLE_DEVICES="0,5"
-N_PROC=2
+SEEDS="0-49999"
+
+BASE_CUDA_VISIBLE_DEVICES="0,5"
+N_PROC=$((1 + ${#BASE_CUDA_VISIBLE_DEVICES} / 2))
 
 BASE_N_STEPS=18
 COMMON_SAVE_DIR=images_with_skips
 
-mkdir $COMMON_SAVE_DIR
+rm -r ${COMMON_SAVE_DIR}; mkdir ${COMMON_SAVE_DIR}
 
 
-N_SKIPS=(0 1 2 3 4 5 6 7 10 12)
+N_SKIPS=(0 1 2 3 4 5 6 7 8 10 12)
 SIGMA_SKIPS=(80.0 57.6 40.8 28.4 19.4 12.9 8.4 5.3 3.3 1.1 0.3)
 for idx in ${!N_SKIPS[*]}
 do
@@ -26,13 +26,15 @@ do
     SIGMA_SKIP=${SIGMA_SKIPS[$idx]}
     
     SAVE_DIR=${COMMON_SAVE_DIR}/${N_SKIP}
-    mkdir SAVE_DIR
 
+    CUDA_VISIBLE_DEVICES=${BASE_CUDA_VISIBLE_DEVICES}
     torchrun --standalone --nproc_per_node=${N_PROC} generate.py \
         --network=${NETWORK} --solver=${SOLVER} --steps=${N_STEPS} --skip_method=${SKIP_METHOD} \
         --sigma_max=${SIGMA_MAX} --sigma_skip=${SIGMA_SKIP} --ds_params_dir=${DS_PARAMS_DIR}\
         --batch=${BATCH} --seeds=${SEEDS} --outdir=${SAVE_DIR}
 
-    python -u fid.py calc --images=${SAVE_DIR} --num=${NUM}\
+    CUDA_VISIBLE_DEVICES=${BASE_CUDA_VISIBLE_DEVICES:0:1}
+    NUM_IMAGES=$(ls -l ${SAVE_DIR}/*.png | wc -l)
+    python -u fid.py calc --images=${SAVE_DIR} --num=${NUM_IMAGES}\
         --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz > ${COMMON_SAVE_DIR}/${N_SKIP}.txt
 done
