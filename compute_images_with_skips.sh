2b514bce9e0921b6981d76cc8aca8d59602e7041
Daniil Shlyonskiy <d-shlenskii@terranova.vla.yp-c.yandex.net>
2023-12-17 19:26:17 +0300

    fid computation added

--- compute_images_with_skips.sh
+++ compute_images_with_skips.sh
@@ -1,15 +1,17 @@
-NETWORK=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl
+NETWORK=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-uncond-vp.pkl
 SOLVER="heun"
 SKIP_METHOD="gaussian"
 SIGMA_MAX=80.0
 DS_PARAMS_DIR="gaussian_params"
 
-BATCH=512
-SEEDS="0-9999"
+BATCH=1024
+SEEDS="0-49999"
+NUM=10000
 
-CUDA_VISIBLE_DEVICES=0
+CUDA_VISIBLE_DEVICES="0,5"
+N_PROC=2
 
-BASE_NFE=35
+BASE_N_STEPS=18
 COMMON_SAVE_DIR=images_with_skips
 
 mkdir $COMMON_SAVE_DIR
@@ -20,14 +22,17 @@ SIGMA_SKIPS=(80.0 57.6 40.8 28.4 19.4 12.9 8.4 5.3 3.3 1.1 0.3)
 for idx in ${!N_SKIPS[*]}
 do
     N_SKIP=${N_SKIPS[$idx]}
-    NFE=$((BASE_NFE - 2 * N_SKIP))
+    N_STEPS=$((BASE_N_STEPS - N_SKIP))
     SIGMA_SKIP=${SIGMA_SKIPS[$idx]}
     
     SAVE_DIR=${COMMON_SAVE_DIR}/${N_SKIP}
     mkdir SAVE_DIR
 
-    python generate.py \
-        --network=${NETWORK} --solver=${SOLVER} --skip_method=${SKIP_METHOD} \
+    torchrun --standalone --nproc_per_node=${N_PROC} generate.py \
+        --network=${NETWORK} --solver=${SOLVER} --steps=${N_STEPS} --skip_method=${SKIP_METHOD} \
         --sigma_max=${SIGMA_MAX} --sigma_skip=${SIGMA_SKIP} --ds_params_dir=${DS_PARAMS_DIR}\
-        --batch=${BATCH} --seeds=${SEEDS} --outdir=${SAVE_DIR} 
+        --batch=${BATCH} --seeds=${SEEDS} --outdir=${SAVE_DIR}
+
+    python -u fid.py calc --images=${SAVE_DIR} --num=${NUM}\
+        --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz > ${COMMON_SAVE_DIR}/${N_SKIP}.txt
 done
